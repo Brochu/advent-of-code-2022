4094307d43bc226622f2042c642a49f155751ac0
Alex Brochu <alexbrochu1@gmail.com>
2022-12-02 00:41:56 -0500

    [P] Completed day2, part 1 and 2

--- src/day2.rs
+++ src/day2.rs
@@ -10,18 +10,120 @@ pub fn main() {
     println!("[Day2] Complete -----------------------");
 }
 
-fn run_part1() -> u64 {
-    let lines: Vec<_> = include_str!("../data/day2.example")
-        .lines()
-        .collect();
+#[derive(Debug)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
 
-    return lines.len() as u64;
+fn parse_shapes(s: (&str, &str)) -> (Shape, Shape) {
+    let other = match s.0 {
+        "A" => Shape::Rock,
+        "B" => Shape::Paper,
+        "C" => Shape::Scissors,
+        _ => panic!("[Day2] Could not parse shape")
+    };
+
+    let yours = match s.1.trim() {
+        "X" => Shape::Rock,
+        "Y" => Shape::Paper,
+        "Z" => Shape::Scissors,
+        _ => panic!("[Day2] Could not parse shape")
+    };
+
+    return (other, yours);
+}
+
+fn find_lose(s: &Shape) -> Shape {
+    return match s {
+        Shape::Rock => Shape::Scissors,
+        Shape::Paper => Shape::Rock,
+        Shape::Scissors => Shape::Paper,
+    };
+}
+
+fn find_tie(s: &Shape) -> Shape {
+    return match s {
+        Shape::Rock => Shape::Rock,
+        Shape::Paper => Shape::Paper,
+        Shape::Scissors => Shape::Scissors,
+    };
+}
+
+fn find_win(s: &Shape) -> Shape {
+    return match s {
+        Shape::Rock => Shape::Paper,
+        Shape::Paper => Shape::Scissors,
+        Shape::Scissors => Shape::Rock,
+    };
+}
+
+fn parse_shapes_part2(s: (&str, &str)) -> (Shape, Shape) {
+    let other = match s.0 {
+        "A" => Shape::Rock,
+        "B" => Shape::Paper,
+        "C" => Shape::Scissors,
+        _ => panic!("[Day2] Could not parse shape")
+    };
+
+    let yours = match s.1.trim() {
+        "X" => find_lose(&other),
+        "Y" => find_tie(&other),
+        "Z" => find_win(&other),
+        _ => panic!("[Day2] Could not parse shape")
+    };
+
+    return (other, yours);
+}
+
+fn calc_outcome(other: Shape, yours: Shape) -> u64 {
+    let (base, win) = match yours {
+        Shape::Rock => {
+            let score = match other {
+                Shape::Rock => 3,
+                Shape::Paper => 0,
+                Shape::Scissors => 6,
+            };
+            (1, score)
+        },
+        Shape::Paper => {
+            let score = match other {
+                Shape::Rock => 6,
+                Shape::Paper => 3,
+                Shape::Scissors => 0,
+            };
+            (2, score)
+        },
+        Shape::Scissors => {
+            let score = match other {
+                Shape::Rock => 0,
+                Shape::Paper => 6,
+                Shape::Scissors => 3,
+            };
+            (3, score)
+        },
+    };
+
+    return base + win;
+}
+
+fn run_part1() -> u64 {
+    return include_str!("../data/day2.input")
+        .lines()
+        .map(|s| parse_shapes(s.split_once(" ").unwrap()))
+        .map(|m| calc_outcome(m.0, m.1))
+        .collect::<Vec<u64>>()
+        .iter()
+        .sum();
 }
 
 fn run_part2() -> u64 {
-    let lines: Vec<_> = include_str!("../data/day2.example")
+    return include_str!("../data/day2.input")
         .lines()
-        .collect();
-
-    return lines.len() as u64;
+        .map(|s| parse_shapes_part2(s.split_once(" ").unwrap()))
+        .map(|m| calc_outcome(m.0, m.1))
+        .collect::<Vec<u64>>()
+        .iter()
+        .sum();
 }
