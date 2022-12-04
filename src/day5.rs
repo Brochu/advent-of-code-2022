23c15a86d32c93f4427a8b30921a0dc99afa9f06
Alex Brochu <alexbrochu1@gmail.com>
2022-12-04 01:26:40 -0500

    [S] More setup work for day 5, should change template

--- src/day5.rs
+++ src/day5.rs
@@ -1,27 +1,20 @@
 pub fn main() {
-    println!("[Day1] Solutions:");
+    println!("[Day5] Solutions:");
 
-    let part1 = run_part1();
-    println!("[Day1] Part 1 => {}", part1);
+    let lines = include_str!("../data/day5.example")
+        .lines()
+        .collect::<Vec<&str>>();
 
-    let part2 = run_part2();
-    println!("[Day1] Part 2 => {}", part2);
+    println!("[Day5] Part 1 => {}", run_part1(&lines));
+    println!("[Day5] Part 2 => {}", run_part2(&lines));
 
-    println!("[Day1] Complete -----------------------");
+    println!("[Day5] Complete -----------------------");
 }
 
-fn run_part1() -> u64 {
-    let lines: Vec<_> = include_str!("../data/day1.example")
-        .lines()
-        .collect();
-
+fn run_part1(lines: &Vec<&str>) -> u64 {
     return lines.len() as u64;
 }
 
-fn run_part2() -> u64 {
-    let lines: Vec<_> = include_str!("../data/day1.example")
-        .lines()
-        .collect();
-
+fn run_part2(lines: &Vec<&str>) -> u64 {
     return lines.len() as u64;
 }
