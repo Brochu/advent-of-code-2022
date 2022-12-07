360306eea0e9f5c2d487549ee63c732ece908629
Alex Brochu <alexbrochu1@gmail.com>
2022-12-07 13:26:23 -0500

    [W] Finished day 7, both parts, I can do better, lots of over complicated structs

--- src/day7.rs
+++ src/day7.rs
@@ -55,22 +55,36 @@ fn build_fs(cmds: Vec<&str>) -> FS {
         }
     }
 
-    //println!();
-    //nodes.iter()
-    //    .for_each(|n| println!("{}: {}", n.name, n.size));
-    //tree.iter()
-    //    .for_each(|n| println!("{}; {:?}", n.index, n.parent));
-
-    calc_dir_sizes(&nodes, &tree);
+    calc_dir_sizes(&mut nodes, &tree);
     return FS { nodes, tree };
 }
 
-fn calc_dir_sizes(nodes: &Vec<FSNode>, tree: &Vec<FSRef>) {
+fn calc_dir_sizes(nodes: &mut Vec<FSNode>, tree: &Vec<FSRef>) {
     calc_dir_sizes_impl(nodes, tree, 0);
 }
 
-fn calc_dir_sizes_impl(nodes: &Vec<FSNode>, tree: &Vec<FSRef>, cur_idx: usize) {
-    //TODO: Algo to depth first through the tree and set dir sizes
+fn calc_dir_sizes_impl(nodes: &mut Vec<FSNode>, tree: &Vec<FSRef>, cur_idx: usize) {
+    let curr_ref = &tree[cur_idx];
+
+    if curr_ref.children.len() <= 0 {
+        return
+    }
+    else {
+        curr_ref.children
+            .iter()
+            .for_each(|&c| calc_dir_sizes_impl(nodes, tree, c));
+
+        let dir_size = curr_ref.children
+            .iter()
+            .map(|&c| {
+                let fs_ref = &tree[c];
+                let other = &nodes[fs_ref.index];
+                other.size
+            })
+            .sum::<usize>();
+
+        nodes[curr_ref.index].size = dir_size;
+    }
 }
 
 fn debug_fs(fs: &FS) {
@@ -82,7 +96,7 @@ fn debug_ref(current: &FSRef, fs: &FS, depth: usize) {
 
     let prefix = (0..depth).map(|_| ' ').collect::<String>();
     if current.children.len() > 0 {
-        println!("{}- {} (dir)", prefix, node.name);
+        println!("{}- {} (dir, size={})", prefix, node.name, node.size);
 
         current.children.iter()
             .for_each(|&i| debug_ref(&fs.tree[i], fs, depth+1));
@@ -95,7 +109,7 @@ fn debug_ref(current: &FSRef, fs: &FS, depth: usize) {
 pub fn main() {
     println!("[Day7] Solutions:");
 
-    let cmds = include_str!("../data/day7.example")
+    let cmds = include_str!("../data/day7.input")
         .lines()
         .collect::<Vec<&str>>();
 
@@ -111,10 +125,49 @@ pub fn main() {
     println!("[Day7] Complete -----------------------");
 }
 
-fn run_part1(_fs: &FS) -> usize {
-    return 0;
+fn run_part1(fs: &FS) -> usize {
+    return fs.tree.iter()
+        .filter_map(|n| {
+            if n.children.len() <= 0 {
+                None
+            }
+            else {
+                let node = &fs.nodes[n.index];
+                if node.size > 100_000 {
+                    None
+                }
+                else {
+                    Some(node.size)
+                }
+            }
+        })
+        .sum();
 }
 
-fn run_part2(_fs: &FS) -> usize {
-    return 0;
+fn run_part2(fs: &FS) -> usize {
+    const SPACE_AVAILABLE: usize = 70_000_000;
+    const SPACE_NEEDED: usize = 30_000_000;
+
+    let root = &fs.nodes[0];
+    let unused = SPACE_AVAILABLE - root.size;
+    let missing = SPACE_NEEDED - unused;
+
+    //println!("unused space = {}; needed = {}; missing = {}", unused, SPACE_NEEDED, missing);
+
+    return fs.tree.iter()
+        .filter_map(|n| {
+            if n.children.len() <= 0 {
+                None
+            }
+            else {
+                let node = &fs.nodes[n.index];
+                if node.size >= missing {
+                    Some(node.size)
+                }
+                else {
+                    None
+                }
+            }
+        })
+        .min().unwrap();
 }
