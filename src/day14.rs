eacb0efe2515f05ce0550725121387b347071033
Alex Brochu <alexbrochu1@gmail.com>
2022-12-14 12:55:21 -0500

    [W] Finished and succeeded part 1 of day 14, cool one

--- src/day14.rs
+++ src/day14.rs
@@ -1,21 +1,53 @@
 use std::collections::HashMap;
 
-#[derive(Clone, Debug)]
+#[derive(Debug, Clone)]
 enum Cell {
+    Spawner,
     Stone,
-    //Sand,
+    Sand,
 }
 
 type Segment = Vec<(u32, u32)>;
-type Map = HashMap<(u32, u32), Cell>;
+
+#[derive(Clone)]
+struct Map {
+    cells: HashMap<(u32, u32), Cell>,
+    min_bounds: (u32, u32),
+    max_bounds: (u32, u32),
+    spawn: (u32, u32),
+}
+
+//impl Map {
+//    pub fn show_map(&self) {
+//        let (x_min, y_min) = self.min_bounds;
+//        let (x_max, y_max) = self.max_bounds;
+//
+//        for y in y_min..=y_max+2 {
+//            for x in x_min-2..=x_max+2 {
+//                let cell = match self.cells.get(&(x, y)) {
+//                    Some(c) => {
+//                        match c {
+//                            Cell::Spawner => '+',
+//                            Cell::Stone => '#',
+//                            Cell::Sand => 'o',
+//                        }
+//                    },
+//                    None => '.',
+//                };
+//                print!("{}", cell);
+//            }
+//            println!();
+//        }
+//    }
+//}
 
 fn build_map(segments: &Vec<Segment>) -> Map {
-    let mut min = (u32::MAX, u32::MAX);
-    let mut max = (u32::MIN, u32::MIN);
-
-    let map = segments.iter()
-        .fold(Map::new(), |mut m, seg| {
-            m.insert(seg[0], Cell::Stone);
+    let mut map = segments.iter()
+        .fold(Map { cells: HashMap::new(),
+                    min_bounds: (u32::MAX, 0),
+                    max_bounds: (0, 0),
+                    spawn: (500, 0) }, |mut m, seg| {
+            m.cells.insert(seg[0], Cell::Stone);
 
             seg[..].windows(2).for_each(|w| {
                 let (x0, y0) = w[0];
@@ -32,14 +64,20 @@ fn build_map(segments: &Vec<Segment>) -> Map {
 
                 for x in xrange {
                     for y in yrange.clone() {
-                        m.insert((x, y), Cell::Stone);
+                        m.cells.insert((x, y), Cell::Stone);
+
+                        if x < m.min_bounds.0 { m.min_bounds.0 = x; }
+                        if x > m.max_bounds.0 { m.max_bounds.0 = x; }
+
+                        if y < m.min_bounds.1 { m.min_bounds.1 = y; }
+                        if y > m.max_bounds.1 { m.max_bounds.1 = y; }
                     }
                 }
             });
             m
         });
 
-    // Find map bounds here?
+    map.cells.insert(map.spawn, Cell::Spawner);
     return map;
 }
 
@@ -58,20 +96,64 @@ pub fn main() {
 
     let map = build_map(&segments);
     println!("[Day14] Part 1 => {}", run_part1(map.clone()));
-    //println!("[Day14] Part 2 => {}", run_part2(&lines));
+    println!("[Day14] Part 2 => {}", run_part2(map.clone()));
 
     println!("[Day14] Complete -----------------------");
 }
 
-fn run_part1(map: Map) -> usize {
-    map.iter()
-        .for_each(|(k, v)| {
-            println!("{:?} -> {:?}", k, v);
-        });
+fn run_part1(mut map: Map) -> usize {
+    //TODO: Find a way to split this, reuse for part 2
+    for _ in 0..100_000 {
+        let (mut x, mut y) = map.spawn;
+        let mut fell = true;
 
-    return map.len();
+        while y < map.max_bounds.1 {
+            let target = match map.cells.get(&(x, y+1)) {
+                None => Some((x, y+1)),
+                Some(_) => {
+                    match map.cells.get(&(x-1, y+1)) {
+                        None => Some((x-1, y+1)),
+                        Some(_) => {
+                            match map.cells.get(&(x+1, y+1)) {
+                                None => Some((x+1, y+1)),
+                                Some(_) => None,
+                            }
+                        },
+                    }
+                },
+            };
+
+            //println!("Sand at ({:?}), falling to ({:?})", (x, y), target);
+            match target {
+                Some((new_x, new_y)) => {
+                    x = new_x;
+                    y = new_y;
+                },
+                None => {
+                    map.cells.insert((x, y), Cell::Sand);
+                    fell = false;
+                    break;
+                }
+            };
+        }
+
+        if fell {
+            break;
+        }
+    }
+
+    //map.show_map();
+    return map.cells.iter()
+        .filter(|&(_, cell)| {
+            match cell {
+                Cell::Sand => true,
+                _ => false,
+            }
+        })
+        .count();
 }
-//
-//fn run_part2(lines: &Vec<&str>) -> usize {
-//    return lines.len();
-//}
+
+fn run_part2(mut _map: Map) -> usize {
+    // Simulate infinite floor at max_bounds.1 + 2
+    return 0;
+}
