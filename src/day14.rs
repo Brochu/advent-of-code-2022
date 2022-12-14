7256c16126352824665cf4fde37313626f0d91da
Alex Brochu <alexbrochu1@gmail.com>
2022-12-14 01:49:17 -0500

    [W] Progress on day 14 part 1, got some parsing going on, remember to clone map into part 1 func to mut. Write a debug viz func

--- src/day14.rs
+++ src/day14.rs
@@ -1,20 +1,77 @@
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+enum Cell {
+    Stone,
+    //Sand,
+}
+
+type Segment = Vec<(u32, u32)>;
+type Map = HashMap<(u32, u32), Cell>;
+
+fn build_map(segments: &Vec<Segment>) -> Map {
+    let mut min = (u32::MAX, u32::MAX);
+    let mut max = (u32::MIN, u32::MIN);
+
+    let map = segments.iter()
+        .fold(Map::new(), |mut m, seg| {
+            m.insert(seg[0], Cell::Stone);
+
+            seg[..].windows(2).for_each(|w| {
+                let (x0, y0) = w[0];
+                let (x1, y1) = w[1];
+
+                let xrange = match x0 < x1 {
+                    true => x0..=x1,
+                    false => x1..=x0,
+                };
+                let yrange = match y0 < y1 {
+                    true => y0..=y1,
+                    false => y1..=y0,
+                };
+
+                for x in xrange {
+                    for y in yrange.clone() {
+                        m.insert((x, y), Cell::Stone);
+                    }
+                }
+            });
+            m
+        });
+
+    // Find map bounds here?
+    return map;
+}
+
 pub fn main() {
     println!("[Day14] Solutions:");
 
-    let lines = include_str!("../data/day14.example")
-        .lines()
-        .collect::<Vec<&str>>();
+    let segments = include_str!("../data/day14.example")
+        .lines().map(|line| {
+            line.split(" -> ").map(|e| {
+                    let (x, y) = e.split_once(",").unwrap();
+                    (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap())
+                })
+                .collect::<Segment>()
+        })
+        .collect::<Vec<Segment>>();
 
-    println!("[Day14] Part 1 => {}", run_part1(&lines));
-    println!("[Day14] Part 2 => {}", run_part2(&lines));
+    let map = build_map(&segments);
+    println!("[Day14] Part 1 => {}", run_part1(map.clone()));
+    //println!("[Day14] Part 2 => {}", run_part2(&lines));
 
     println!("[Day14] Complete -----------------------");
 }
 
-fn run_part1(lines: &Vec<&str>) -> usize {
-    return lines.len();
-}
+fn run_part1(map: Map) -> usize {
+    map.iter()
+        .for_each(|(k, v)| {
+            println!("{:?} -> {:?}", k, v);
+        });
 
-fn run_part2(lines: &Vec<&str>) -> usize {
-    return lines.len();
+    return map.len();
 }
+//
+//fn run_part2(lines: &Vec<&str>) -> usize {
+//    return lines.len();
+//}
