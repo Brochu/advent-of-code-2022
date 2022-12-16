4e9a2a5934003f89dc2c1913d0707f25842ac4f6
Alex Brochu <alexbrochu1@gmail.com>
2022-12-15 23:17:29 -0500

    [W] Progress on day 15, got part 1 correct, part 2 brute force doesn't work

--- src/day15.rs
+++ src/day15.rs
@@ -1,20 +1,152 @@
+//#[derive(Debug)]
+struct Sensor {
+    pos: (i64, i64),
+    //beacon: (i64, i64),
+    range: u64,
+}
+
+type Map = Vec<Sensor>;
+
+//fn draw_map(map: &Map) {
+//    let (mut x_min, mut x_max) = (i64::MAX, 0);
+//    let (mut y_min, mut y_max) = (i64::MAX, 0);
+//
+//    map.iter()
+//        .for_each(|s| {
+//            let (pos_x, pos_y) = s.pos;
+//            let (b_x, b_y) = s.beacon;
+//
+//            let x_mins = vec![x_min, pos_x, b_x];
+//            let y_mins = vec![y_min, pos_y, b_y];
+//
+//            x_min = *x_mins.iter().min().unwrap();
+//            y_min = *y_mins.iter().min().unwrap();
+//
+//            let x_maxs = vec![x_max, pos_x, b_x];
+//            let y_maxs = vec![y_max, pos_y, b_y];
+//
+//            x_max = *x_maxs.iter().max().unwrap();
+//            y_max = *y_maxs.iter().max().unwrap();
+//        });
+//
+//    for y in y_min..=y_max {
+//        for x in x_min..=x_max {
+//            let c = (x, y);
+//            let found = map.iter().filter(|s| s.pos == c || s.beacon == c).next();
+//
+//            let out = match found {
+//                Some(s) => {
+//                    if s.pos == c { "S" } else { "B" }
+//                },
+//                None => ".",
+//            };
+//
+//            print!("{out}");
+//        }
+//        println!();
+//    }
+//}
+
 pub fn main() {
     println!("[Day15] Solutions:");
+    let (input, row) = (include_str!("../data/day15.example"), 10);
+    //let (input, row) = (include_str!("../data/day15.input"), 2_000_000);
 
-    let lines = include_str!("../data/day15.example")
-        .lines()
-        .collect::<Vec<&str>>();
+    let map = input.lines()
+        .map(|line| {
+            // Is there a way to make this less ugly??
+            let (sensor_str, beacon_str) = line.split_once(": ").unwrap();
+            let (_, pos_str) = sensor_str.split_once("at ").unwrap();
+            let (_, closest_str) = beacon_str.split_once("at ").unwrap();
 
-    println!("[Day15] Part 1 => {}", run_part1(&lines));
-    println!("[Day15] Part 2 => {}", run_part2(&lines));
+            let (x_str, y_str) = pos_str.split_once(", ").unwrap();
+            let (_, x) = x_str.split_once("=").unwrap();
+            let (_, y) = y_str.split_once("=").unwrap();
+            let pos = (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap());
+
+            let (x_str, y_str) = closest_str.split_once(", ").unwrap();
+            let (_, x) = x_str.split_once("=").unwrap();
+            let (_, y) = y_str.split_once("=").unwrap();
+            let beacon = (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap());
+
+            Sensor { pos, /*beacon,*/ range: ((pos.0 - beacon.0).abs() + (pos.1 - beacon.1).abs()) as u64 }
+        })
+    .collect::<Map>();
+
+    //map.iter()
+    //    .for_each(|s| println!("pos: {:?}, beacon: {:?}, range: {}", s.pos, s.beacon, s.range));
+
+    println!("[Day15] Part 1 => {}", run_part1(&map, row));
+    println!("[Day15] Part 2 => {}", run_part2(&map));
 
     println!("[Day15] Complete -----------------------");
 }
 
-fn run_part1(lines: &Vec<&str>) -> usize {
-    return lines.len();
+fn cover_ranges(map: &Map, row: i64) -> Vec<(i64, i64)> {
+    //draw_map(map);
+    let mut ranges =  map.iter()
+        .filter_map(|s| {
+            let (x, y) = s.pos;
+            let dist = (y - row).abs() as u64;
+
+            if  dist <= s.range {
+                let diff = (s.range - dist) as i64;
+                Some((x - diff, x + diff))
+            }
+            else {
+                None
+            }
+        })
+    .collect::<Vec<(i64, i64)>>();
+
+    ranges.sort_by(|&(l_start, _), &(r_start, _)| l_start.cmp(&r_start));
+    return ranges;
 }
 
-fn run_part2(lines: &Vec<&str>) -> usize {
-    return lines.len();
+fn run_part1(map: &Map, row: i64) -> usize {
+    let (covered_min, covered_max) = cover_ranges(map, row)
+        .iter()
+        .fold((i64::MAX, i64::MIN), |(final_min, final_max), &(current_min, current_max)| {
+            (
+                if current_min < final_min { current_min } else { final_min },
+                if current_max > final_max { current_max } else { final_max },
+            )
+        });
+
+    return (covered_min..covered_max).count();
+}
+
+fn _find_beacon(map: &Map) -> (i64, i64) {
+    //const MAX: i64 = 20;
+    const MAX: i64 = 4_000_000;
+
+    // THIS DOES NOT WORK, takes too long, work with merged ranges instead
+    for row in 0..=MAX {
+        let ranges = cover_ranges(map, row);
+
+        for x in 0..=MAX {
+
+            if !ranges.iter().map(|&(min, max)| min..=max).any(|r| r.contains(&(x as i64))) {
+                return (x, row);
+            }
+        }
+    }
+
+    return (-1, -1);
+}
+
+fn run_part2(map: &Map) -> i64 {
+    //let (beacon_x, beacon_y) = find_beacon(map);
+    let ranges = cover_ranges(map, 10);
+    ranges.iter()
+        .for_each(|r| println!("{:?}", r));
+
+    println!();
+
+    let ranges = cover_ranges(map, 11);
+    ranges.iter()
+        .for_each(|r| println!("{:?}", r));
+
+    let (beacon_x, beacon_y) = (0, 0);
+    return (beacon_x * 4_000_000) + beacon_y;
 }
