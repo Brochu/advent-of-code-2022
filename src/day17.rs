d1ffb09815ae46af978e39a59ba7e387c0583445
Alex Brochu <alexbrochu1@gmail.com>
2022-12-19 04:21:09 -0500

    [W] Progress on day 17, part 1. Got the rocks and status, needs simulation next

--- src/day17.rs
+++ src/day17.rs
@@ -1,17 +1,12 @@
+use std::collections::HashSet;
+
 #[derive(Debug)]
 enum Direction {
     Left,
     Right,
 }
 
-#[derive(Debug)]
-enum Shape {
-    HLine,
-    Cross,
-    Corner,
-    VLine,
-    Block,
-}
+type Shape = Vec<(u32, u32)>;
 
 pub fn main() {
     println!("[Day17] Solutions:");
@@ -29,7 +24,13 @@ pub fn main() {
         })
         .collect::<Vec<Direction>>();
 
-    let shapes: Vec<Shape> = vec![Shape::HLine, Shape::Cross, Shape::Corner, Shape::VLine, Shape::Block];
+    let shapes: Vec<Shape> = vec![
+        vec![(2, 3), (3, 3), (4, 3), (5, 3)],
+        vec![(2, 4), (3, 4), (4, 4), (3, 5), (3, 3)],
+        vec![(2, 3), (3, 3), (4, 3), (4, 4), (4, 5)],
+        vec![(2, 3), (2, 4), (2, 5), (2, 6)],
+        vec![(2, 3), (3, 3), (2, 4), (3, 4)],
+    ];
 
     println!("[Day17] Part 1 => {}", run_part1(&push_dirs, &shapes));
     //println!("[Day17] Part 2 => {}", run_part2());
@@ -37,17 +38,47 @@ pub fn main() {
     println!("[Day17] Complete -----------------------");
 }
 
-fn run_part1(dirs: &Vec<Direction>, shapes: &Vec<Shape>) -> u32 {
-    println!("{:?}", dirs);
-    let mut dir_cycle = dirs[0..5].iter().cycle();
-    let mut shape_cycle = shapes.iter().cycle();
+fn show_map(status: &HashSet<(u32, u32)>, rock: &Shape, height: u32, width: u32) {
+    println!();
 
-    for i in 0..15 {
-        println!("[{i}] : {:?}", dir_cycle.next().unwrap());
+    for y in (0..height).rev() {
+        for x in 0..width {
+            if status.contains(&(x, y)) { print!("#"); }
+            else if rock.iter().any(|&p| p == (x, y)) { print!("@"); }
+            else { print!("."); }
+        }
+        println!();
     }
     println!();
-    for i in 0..15 {
-        println!("[{i}] : {:?}", shape_cycle.next().unwrap());
+}
+
+fn run_part1(dirs: &Vec<Direction>, shapes: &Vec<Shape>) -> u32 {
+    //println!("{:?}", dirs);
+    //println!("{:?}", shapes);
+    //let mut dir_cycle = dirs[0..5].iter().cycle();
+    let mut shape_cycle = shapes.iter().cycle();
+
+    //for i in 0..15 {
+    //    println!("[{i}] : {:?}", dir_cycle.next().unwrap());
+    //}
+    //println!();
+    //for i in 0..15 {
+    //    println!("[{i}] : {:?}", shape_cycle.next().unwrap());
+    //}
+
+    const WIDTH: u32 = 7;
+    let mut height: u32 = 8;
+    let mut status = HashSet::<(u32, u32)>::new();
+
+    for _ in 0..1 {
+        let mut rock = shape_cycle.next().unwrap().clone();
+        show_map(&status, &rock, height, WIDTH);
+
+        //TODO Apply direction push
+        //  Check for collide with walls 0 and 7
+        //TODO Rock falls
+        //  Check for collide with bottom + other rocks
+        //  Add rock to status when collision happens
     }
 
     return 0;
