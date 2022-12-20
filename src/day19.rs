988c2985ee04bbf4bc2f53d512978cbb522023a2
Alex Brochu <alexbrochu1@gmail.com>
2022-12-19 22:48:45 -0500

    [S] Prepare for puzzle of day 19

---
+++ src/day19.rs
@@ -0,0 +1,20 @@
+pub fn main() {
+    println!("[Day0] Solutions:");
+
+    let lines = include_str!("../data/day0.example")
+        .lines()
+        .collect::<Vec<&str>>();
+
+    println!("[Day0] Part 1 => {}", run_part1());
+    //println!("[Day0] Part 2 => {}", run_part2());
+
+    println!("[Day0] Complete -----------------------");
+}
+
+fn run_part1() -> usize {
+    return 0;
+}
+
+//fn run_part2() -> usize {
+//    return 0;
+//}
