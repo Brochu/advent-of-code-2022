4992593ab9ff7bdc32b1a7afb8451678c3782613
Alex Brochu <alexbrochu1@gmail.com>
2022-12-19 22:46:47 -0500

    [S] Better Template for daily puzzles

--- src/template.rs
+++ src/template.rs
@@ -5,16 +5,16 @@ pub fn main() {
         .lines()
         .collect::<Vec<&str>>();
 
-    println!("[Day0] Part 1 => {}", run_part1(&lines));
-    println!("[Day0] Part 2 => {}", run_part2(&lines));
+    println!("[Day0] Part 1 => {}", run_part1());
+    //println!("[Day0] Part 2 => {}", run_part2());
 
     println!("[Day0] Complete -----------------------");
 }
 
-fn run_part1(lines: &Vec<&str>) -> usize {
-    return lines.len();
+fn run_part1() -> usize {
+    return 0;
 }
 
-fn run_part2(lines: &Vec<&str>) -> usize {
-    return lines.len();
-}
+//fn run_part2() -> usize {
+//    return 0;
+//}
