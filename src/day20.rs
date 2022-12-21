bf4b0e7a7555036630eece033df0cca91ecd9cae
Alex Brochu <alexbrochu1@gmail.com>
2022-12-21 04:56:50 -0500

    [S] Setup and prepare for day 20, when 19 works

---
+++ src/day20.rs
@@ -0,0 +1,20 @@
+pub fn main() {
+    println!("[Day20] Solutions:");
+
+    let lines = include_str!("../data/day20.example")
+        .lines()
+        .collect::<Vec<&str>>();
+
+    println!("[Day20] Part 1 => {}", run_part1());
+    //println!("[Day20] Part 2 => {}", run_part2());
+
+    println!("[Day20] Complete -----------------------");
+}
+
+fn run_part1() -> usize {
+    return 0;
+}
+
+//fn run_part2() -> usize {
+//    return 0;
+//}
