db7805570f8050fd3fab5a851417715090a0fefa
Alex Brochu <alexbrochu1@gmail.com>
2022-12-24 12:35:34 -0500

    [S] Setup for day 24 puzzle

---
+++ src/day24.rs
@@ -0,0 +1,20 @@
+pub fn main() {
+    println!("[Day24] Solutions:");
+
+    let lines = include_str!("../data/day24.example")
+        .lines()
+        .collect::<Vec<&str>>();
+
+    println!("[Day24] Part 1 => {}", run_part1());
+    //println!("[Day24] Part 2 => {}", run_part2());
+
+    println!("[Day24] Complete -----------------------");
+}
+
+fn run_part1() -> usize {
+    return 0;
+}
+
+//fn run_part2() -> usize {
+//    return 0;
+//}
