10c1174e8e43d61b0cc83fc152e169c76504e2dd
Alex Brochu <alexbrochu1@gmail.com>
2022-12-26 02:51:39 -0500

    [W] Started part 2 for day 16, we need to handle elephant position as well

--- src/day16.rs
+++ src/day16.rs
@@ -112,9 +112,41 @@ fn best_pressure(net: &Network, pos: usize, open_field: u64, time: u64, cache: &
     }
 }
 
-fn best_pressure_part2(_net: &Network, _pos: usize, _open_field: u64, _time: u64, _cache: &mut Cache) -> u64{
-    //TODO: Figure out how to adapt this to two actors opening valves
-    return 0;
+fn best_pressure_part2(net: &Network, my_pos: usize, ele_pos: usize, open_field: u64, time: u64, cache: &mut Cache) -> u64{
+    if let Some(&val) = cache.get(&(my_pos, open_field, time)) {
+        return val;
+    }
+
+    if time == 1 {
+        let res = calc_pressure(net, open_field);
+        cache.insert((my_pos, open_field, time), res);
+        return res;
+    }
+    else {
+        let v = &net.valves[my_pos];
+        let should_open = v.flow > 0;
+        let on = open_field & (1 << my_pos) > 0;
+
+        let mut options = Vec::<u64>::new();
+
+        if !on && should_open {
+            options.push(best_pressure(net, my_pos, open_field | (1 << my_pos), time-1, cache));
+        }
+
+        v.tunnels.iter()
+            .map(|t| net.lut[t])
+            .for_each(|idx| {
+                options.push(best_pressure(net, idx, open_field, time-1, cache));
+            });
+
+        let res = match options.iter().max() {
+            Some(&val) => val,
+            None => best_pressure(net, my_pos, open_field, time-1, cache),
+        } + calc_pressure(net, open_field);
+
+        cache.insert((my_pos, open_field, time), res);
+        return res;
+    }
 }
 
 fn run_part1(net: &Network) -> u64 {
@@ -131,8 +163,8 @@ fn run_part2(net: &Network) -> u64 {
 
     let start_pos: usize = net.lut[&String::from("AA")];
     const INIT_FIELD: u64 = 0;
-    const INIT_TIME: u64 = 30;
+    const INIT_TIME: u64 = 26;
     let mut cache = Cache::new();
 
-    return best_pressure_part2(net, start_pos, INIT_FIELD, INIT_TIME, &mut cache);
+    return best_pressure_part2(net, start_pos, start_pos, INIT_FIELD, INIT_TIME, &mut cache);
 }
